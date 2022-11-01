d4f30ef831a5c113126de0e3c2c21b3ffb0bad57
manhnguyen2909z <manhnguyenduc2999@gmail.com>
2022-11-01 16:25:00 +0700

    add: fe

---
+++ src/app/user/list-discount/list-discount.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-discount',
+  templateUrl: './list-discount.component.html',
+  styleUrls: ['./list-discount.component.css']
+})
+export class ListDiscountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/user/modal-order/modal-order.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-order',
+  templateUrl: './modal-order.component.html',
+  styleUrls: ['./modal-order.component.css']
+})
+export class ModalOrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
