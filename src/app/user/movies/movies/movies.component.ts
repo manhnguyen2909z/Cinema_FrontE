319191fb3b6b9e81fabdccc89a7293fbfbc8a60d
tienanh189 <letienanh1892001@gmail.com>
2022-11-06 19:46:00 +0700

    6/11: Update code for movies components issue

--- src/app/user/movies/movies/movies.component.ts
+++ src/app/user/movies/movies/movies.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MoviesService } from '../../../services/api/user/movie.service';
-import { moviedto } from 'src/app/services/model/moviedto';
+import { Moviedto } from 'src/app/services/model/moviedto';
 
 @Component({
     selector: 'app-movies',
@@ -8,53 +8,20 @@ import { moviedto } from 'src/app/services/model/moviedto';
     styleUrls: ['./movies.component.css'],
 })
 export class MoviesComponent implements OnInit {
-    // movieDto: Array<moviedto> = new Array<moviedto>
-    // movieDto = [
-    //     {
-    //         image:'https://cdn.galaxycine.vn/media/2022/10/12/1350wx900h_1665592999797.jpg',
-    //         movieId:'1'
-    //     },
-    //     {
-    //         image:'https://cdn.galaxycine.vn/media/2022/10/12/1350wx900h_1665592999797.jpg',
-    //         movieId:'2'
-    //     },
-    //     {
-    //         image:'https://cdn.galaxycine.vn/media/2022/10/12/1350wx900h_1665592999797.jpg',
-    //         movieId:'3'
-    //     },
-    //     {
-    //         image:'https://cdn.galaxycine.vn/media/2022/10/12/1350wx900h_1665592999797.jpg',
-    //         movieId:'4'
-    //     },
-    //     {
-    //         image:'https://cdn.galaxycine.vn/media/2022/10/12/1350wx900h_1665592999797.jpg',
-    //         movieId:'5'
-    //     },
-    //     {
-    //         image:'https://cdn.galaxycine.vn/media/2022/10/12/1350wx900h_1665592999797.jpg',
-    //         movieId:'1'
-    //     },
-    //     {
-    //         image:'https://cdn.galaxycine.vn/media/2022/10/12/1350wx900h_1665592999797.jpg',
-    //         movieId:'1'
-    //     },
-    // ]
     p: number = 1;
-
     constructor(private movie: MoviesService) {}
-    movieDto: moviedto = new moviedto();
+    movieDto: any;
     detailMovieDto: any;
 
     detail(id: string) {
         this.movie.getDetailMovie(id).subscribe((res) => {
             this.detailMovieDto = res;
-            // console.log(id);
         });
     }
     ngOnInit(): void {
         this.movie.getAllMovies().subscribe((res) => {
-            this.movieDto.data = res.data;
-            console.log(res)
+            this.movieDto = res;
+            console.log(this.movieDto)
         });
     }
 }
