0b80050688b6ff57c90118842a76ca34a4890418
manhnguyen2909z <manhnguyenduc2999@gmail.com>
2022-11-11 20:09:32 +0700

    update: payment

--- src/app/shared/sign-in/sign-in.component.ts
+++ src/app/shared/sign-in/sign-in.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-sign-in',
@@ -8,7 +8,6 @@ import { Component, OnInit } from '@angular/core';
 export class SignInComponent implements OnInit {
 
   constructor() { }
-
   ngOnInit(): void {
   }
 
